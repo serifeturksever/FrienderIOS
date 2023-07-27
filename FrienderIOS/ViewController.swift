b85ff8a99be5a2b0e2bf5564d0371dbd4b6d0fe3
serifeturksever <62253012+serifeturksever@users.noreply.github.com>
2023-07-27 15:41:54 +0300

    Initial Commit

---
+++ FrienderIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrienderIOS
+//
+//  Created by Şerife Türksever on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
