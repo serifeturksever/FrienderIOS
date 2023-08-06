5f8a918ed1b6106ab6f13f3350d7923a1a79e32a
serifeturksever <62253012+serifeturksever@users.noreply.github.com>
2023-08-06 21:21:51 +0300

    .

--- FrienderIOS/Controller/CategoryRoomsViewController.swift
+++ FrienderIOS/Controller/CategoryRoomsViewController.swift
@@ -78,7 +78,7 @@ extension CategoryRoomsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.size.width, height: 100)
     }
-    edi
+    
 }
 
 extension CategoryRoomsViewController: UICollectionViewDelegate {
